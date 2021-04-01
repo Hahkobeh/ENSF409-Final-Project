8b93b2ceb659a1843a77c1c82340290e835eba21
Colin Christophe <colin.christophe@ucalgary.ca>
2021-04-01 13:44:04 -0600

    Classes

---
+++ edu/ucalgary/ensf409/FurnitureItem.java
@@ -0,0 +1,12 @@
+package edu.ucalgary.ensf409;
+
+public class FurnitureItem {
+    private String type;
+    private int IDNumber;
+    private int price;
+    private Manufacturer manufacturer;
+    
+
+
+
+}
