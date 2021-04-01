a2b970cb27a60cddd5362ffc20c98d7b8e162aa3
Colin Christophe <colin.christophe@ucalgary.ca>
2021-04-01 13:45:56 -0600

    Furniture class

--- edu/ucalgary/ensf409/FurnitureItem.java
+++ edu/ucalgary/ensf409/FurnitureItem.java
@@ -5,6 +5,30 @@ public class FurnitureItem {
     private int IDNumber;
     private int price;
     private Manufacturer manufacturer;
+    public Manufacturer getManufacturer() {
+        return manufacturer;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public int getIDNumber() {
+        return IDNumber;
+    }
+    public void setIDNumber(int iDNumber) {
+        this.IDNumber = iDNumber;
+    }
+    public int getPrice() {
+        return price;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    public void setManufacturer(Manufacturer manufacturer) {
+        this.manufacturer = manufacturer;
+    }
     
 
 
