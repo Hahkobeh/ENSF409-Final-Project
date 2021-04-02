cc91564c7cf8cbfa2d45af34ec4ec80fa912f37a
Hahkobeh <hahkobeh@gmail.com>
2021-04-01 23:23:59 -0600

    Jacob Update

--- edu/ucalgary/ensf409/Request.java
+++ edu/ucalgary/ensf409/Request.java
@@ -1,33 +1,33 @@
 package edu.ucalgary.ensf409;
 
+import com.mysql.cj.jdbc.exceptions.MysqlDataTruncation;
+
 import java.util.*;
 import java.sql.*;
 
 public class Request {
     private Connection dbConnect;
-    private List<Object> [] items;
     private String category;
     private String type;
     private int size;
     private String usernameMySQL;
     private String passwordMySQL;
+    private char [][] data;
+    private int [] priceData;
+    private String [] dataID;
+    private int numberOfitemsDemanded;
+    private int possibleNumberOfItems;
 
-    public Request(String category, String type, int numberOfItems, String usernameMySQL, String passwordMySQL) {
+    public Request(String category, String type, int numberOfitemsDemanded, String usernameMySQL, String passwordMySQL) {
         this.category = category;
         this.type = type;
-
+        this.numberOfitemsDemanded = numberOfitemsDemanded;
         this.usernameMySQL = usernameMySQL;
         this.passwordMySQL = passwordMySQL;
-
-        items = new List[numberOfItems];
-
-
         setSize(category);
-
-        for(int i = 0; i < items.length; i++){
-            items[i] = new ArrayList<>(size);
-        }
         getDatabase();
+        storeData();
+        totalItemsThatCanBeMade();
 
     }
 
@@ -53,6 +53,108 @@ public class Request {
         }
     }
 
-    public void getLowestPrice(){
+    public void storeData(){
+        int numberOfEntries = 0;
+        String query1 = "SELECT COUNT(*) FROM " + category + " WHERE Type = '" + type + "';";
+
+        try{
+            Statement statement = dbConnect.createStatement();
+
+            ResultSet result = statement.executeQuery(query1);
+            result.next();
+            numberOfEntries = result.getInt("COUNT(*)");
+            result.close();
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+
+        data = new char[numberOfEntries][size];
+        priceData = new int[numberOfEntries];
+        dataID = new String[numberOfEntries];
+
+
+        String query2 = "SELECT * FROM " + category + " WHERE Type = '" + type + "';";
+
+        try{
+            Statement statement = dbConnect.createStatement();
+            ResultSet results = statement.executeQuery(query2);
+            int row = 0;
+
+            while(results.next()){
+                switch (category) {
+                    case "Chair" -> {
+                        data[row][0] = results.getString("Legs").charAt(0);
+                        data[row][1] = results.getString("Arms").charAt(0);
+                        data[row][2] = results.getString("Seat").charAt(0);
+                        data[row][3] = results.getString("Cushion").charAt(0);
+                        priceData[row] = results.getInt("Price");
+                        dataID[row] = results.getString("ID");
+                    }
+                    case "Desk" -> {
+                        data[row][0] = results.getString("Legs").charAt(0);
+                        data[row][1] = results.getString("Top").charAt(0);
+                        data[row][2] = results.getString("Drawer").charAt(0);
+                        priceData[row] = results.getInt("Price");
+                        dataID[row] = results.getString("ID");
+                    }
+                    case "Filing" -> {
+                        data[row][0] = results.getString("Rails").charAt(0);
+                        data[row][1] = results.getString("Drawers").charAt(0);
+                        data[row][2] = results.getString("Cabinet").charAt(0);
+                        priceData[row] = results.getInt("Price");
+                        dataID[row] = results.getString("ID");
+                    }
+                    case "Lamp" -> {
+                        data[row][0] = results.getString("Base").charAt(0);
+                        data[row][1] = results.getString("Bulb").charAt(0);
+                        priceData[row] = results.getInt("Price");
+                        dataID[row] = results.getString("ID");
+                    }
+                    default -> throw new IllegalStateException("Unexpected value: " + category);
+                }
+                row++;
+            }
+            System.out.println("finished");
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+
+
+
     }
+
+    //need a function that checks if possible (enough Y-s in each column)
+
+    public void totalItemsThatCanBeMade(){
+        int possibleNumberOfItems = 10000;
+        int temp;
+        int row;
+        for(int column = 0; column < size; column++){
+            temp = 0;
+            row = 0;
+            while(row < data.length){
+                if(data[row][column]== 'Y'){
+                    temp += 1;
+                }
+                row++;
+            }
+            if(temp < possibleNumberOfItems){
+                possibleNumberOfItems = temp;
+            }
+
+        }
+        this.possibleNumberOfItems = possibleNumberOfItems;
+    }
+
+
+
+
+    //need a function that will be called at the end to remove all used database members
+
+    //I need to find cheapest combination (make note of which
+    //furniture items have been used), then set used parts to N and set price of the furniture item
+    //that has been used to 0 then find next cheapest again.
+
 }
