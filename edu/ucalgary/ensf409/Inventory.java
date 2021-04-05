376ef96205ae4464d66c449e881ae490c9a36f58
BrianKrameruc <73033196+BrianKrameruc@users.noreply.github.com>
2021-04-05 17:20:33 -0600

    Update Inventory.java

--- edu/ucalgary/ensf409/Inventory.java
+++ edu/ucalgary/ensf409/Inventory.java
@@ -1,8 +1,14 @@
+/**
+ @author Colin Christophe <a href="mailto:colin.christophe@ucalgary.ca">colin.christophe@ucalgary.ca</a>
+         Nick Knapton <a href="mailto:nick.knapton@ucalgary.ca">nick.knapton@ucalgary.ca</a>
+         Brian Kramer <a href="mailto:brian.kramer@ucalgary.ca">brian.kramer@ucalgary.ca</a>
+         tucan jake <a href="mailto:tucan.jake@ucalgary.ca">tucan.jake@ucalgary.ca</a>
+ @version       1.5
+ @since         1.0
+ */
 package edu.ucalgary.ensf409;
 
-/**
- * Code written by: Jacob Artuso, Colin Christophe, Nicholas Knapton, and Brian Kramer.
- */
+
 public class Inventory {
     public static void main (String [] args){
         UserInterface ui = new UserInterface();
