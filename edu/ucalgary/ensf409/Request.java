a879400aab2611e66b77538bbff0286a64f8806a
Jacob <hahkobeh@gmail.com>
2021-04-05 16:01:53 -0600

    Jacob Update

--- edu/ucalgary/ensf409/Request.java
+++ edu/ucalgary/ensf409/Request.java
@@ -4,7 +4,6 @@ package edu.ucalgary.ensf409;
  * Code written by: Jacob Artuso, Colin Christophe, Nicholas Knapton, and Brian Kramer
  */
 
-import com.mysql.cj.jdbc.exceptions.MysqlDataTruncation;
 
 import java.util.*;
 
