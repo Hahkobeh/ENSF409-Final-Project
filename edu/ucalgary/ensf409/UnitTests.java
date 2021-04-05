9a88bca7ce1bbaabcd718b06bae0c358d67f2bf2
Colin-C32 <73672795+Colin-C32@users.noreply.github.com>
2021-04-05 17:43:37 -0600

    Create UnitTests.java

--- edu/ucalgary/ensf409/UnitTests.java
+++ edu/ucalgary/ensf409/UnitTests.java
@@ -1,8 +1,8 @@
 /**
  @author Colin Christophe <a href="mailto:colin.christophe@ucalgary.ca">colin.christophe@ucalgary.ca</a>
-         Nick Knapton <a href="mailto:nicholas.knapton@ucalgary.ca">nicholas.knapton@ucalgary.ca</a>
+         Nick Knapton <a href="mailto:nick.knapton@ucalgary.ca">nick.knapton@ucalgary.ca</a>
          Brian Kramer <a href="mailto:brian.kramer@ucalgary.ca">brian.kramer@ucalgary.ca</a>
-         Jacob Artuso <a href="mailto:jacob.artuso@ucalgary.ca">jacob.artuso@ucalgary.ca</a>
+         tucan jake <a href="mailto:tucan.jake@ucalgary.ca">tucan.jake@ucalgary.ca</a>
  @version       1.5
  @since         1.0
  */
@@ -15,5 +15,16 @@ import java.io.*;
 import java.util.*;
 
 public class UnitTests {
+ Request trial = new Request(Chair, Task, 1, false,  "scm","ensf409");
+ OrderForm trialOrder = new OrderForm(trial);
+ assertTrue("orderform.txt".exists());
+ assertTrue(trial.getNumberOfItemsDemanded() == trial.geAmountFilled());
+ trial = new Request(Chair, Task, 1, true,  "scm","ensf409");
+ assertTrue(trial.getNumberOfItemsDemanded() == trial.geAmountFilled());
+ 
+ 
+ 
+ 
+ 
 
 }
