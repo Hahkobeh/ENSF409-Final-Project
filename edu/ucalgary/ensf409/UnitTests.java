7c33f208076990d851504ebe1ada62f0de9b9e9a
Colin-C32 <73672795+Colin-C32@users.noreply.github.com>
2021-04-05 18:09:23 -0600

    Update UnitTests.java

--- edu/ucalgary/ensf409/UnitTests.java
+++ edu/ucalgary/ensf409/UnitTests.java
@@ -18,19 +18,36 @@ public class UnitTests {
  
  @Test
  public void testFileisMade(){
-   Request trial = new Request(Chair, Task, 1, false,  "scm","ensf409"); // full order
+ Request trial = new Request(Chair, Task, 1, false,  "scm","ensf409"); // full order
  OrderForm trialOrder = new OrderForm(trial); // orderform
  assertTrue("orderform.txt".exists()); // check if orderform creates file
  }
  
-
+ @Test 
+ public void testFullOrderIsFilled(){
+ Request trial = new Request(Chair, Task, 1, false,  "scm","ensf409"); // full order
+ OrderForm trialOrder = new OrderForm(trial); // orderform
  assertTrue(trial.getNumberOfItemsDemanded() == trial.geAmountFilled()); //if full order is filled
- trial = new Request(Chair, Task, 1, true,  "scm","ensf409"); // new order of task chairs
- assertTrue(trial.getNumberOfItemsDemanded() == trial.geAmountFilled()); // check is partial order is filled
- trial = new Request(Desk, Standing, 2, false,  "scm","ensf409");
- assertTrue(trial.getPrice() == 600);
- trial = new Request(Lamp, Swing Arm, 3, true,  "scm","ensf409");
- assertTrue(trial.getAmountFilled > 0 && trial.getPartialOrder());
+  
+ }
+  @Test
+ public void testPartialOrderisFilled(){
+  Request Trial = new Request(Chair, Task, 16, true,  "scm","ensf409"); 
+   assertTrue(trial.getAmountFilled > 0 && trial.getPartialOrder());
+ }
+ @Test
+ public void testFullOrderCannotBeFilled(){
+  Request Trial = new Request(Chair, Task, 16, false,  "scm","ensf409"); 
+  assertTrue(trial.getNumberOfItemsDemanded != trial.getAmountFilled();
+ }
+ 
+ @Test 
+ public void testPartialOrderCannotBeFilled(){
+  
+  
+ }
+
+
  
 
 }
