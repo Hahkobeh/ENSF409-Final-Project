d91c971e553a923d461394f1fb0cda1784fab8c2
BrianKrameruc <73033196+BrianKrameruc@users.noreply.github.com>
2021-04-06 19:29:41 -0600

    Update UnitTests.java

--- edu/ucalgary/ensf409/UnitTests.java
+++ edu/ucalgary/ensf409/UnitTests.java
@@ -2,7 +2,7 @@
  @author Colin Christophe <a href="mailto:colin.christophe@ucalgary.ca">colin.christophe@ucalgary.ca</a>
          Nick Knapton <a href="mailto:nick.knapton@ucalgary.ca">nick.knapton@ucalgary.ca</a>
          Brian Kramer <a href="mailto:brian.kramer@ucalgary.ca">brian.kramer@ucalgary.ca</a>
-         tucan jake <a href="mailto:tucan.jake@ucalgary.ca">tucan.jake@ucalgary.ca</a>
+         Jacob Artuso <a href="mailto:jacob.artuso@ucalgary.ca">jacob.artuso@ucalgary.ca</a>
  @version       1.5
  @since         1.0
  */
