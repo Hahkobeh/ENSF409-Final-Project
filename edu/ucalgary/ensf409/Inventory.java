9f125fc14214e0000bf9d2f6c76fc21b54ba843d
Nick <nickknapton12@gmail.com>
2021-04-07 12:10:51 -0600

    Documentation

--- edu/ucalgary/ensf409/Inventory.java
+++ edu/ucalgary/ensf409/Inventory.java
@@ -1,9 +1,9 @@
 /**
  @author Colin Christophe <a href="mailto:colin.christophe@ucalgary.ca">colin.christophe@ucalgary.ca</a>
-         Nick Knapton <a href="mailto:nicholas.knapton@ucalgary.ca">nicholas.knapton@ucalgary.ca</a>
+         Nicholas Knapton <a href="mailto:nicholas.knapton@ucalgary.ca">nicholas.knapton@ucalgary.ca</a>
          Brian Kramer <a href="mailto:brian.kramer@ucalgary.ca">brian.kramer@ucalgary.ca</a>
          Jacob Artuso <a href="mailto:jacob.artuso@ucalgary.ca">jacob.artuso@ucalgary.ca</a>
- @version       1.5
+ @version       1.1
  @since         1.0
  */
 package edu.ucalgary.ensf409;
